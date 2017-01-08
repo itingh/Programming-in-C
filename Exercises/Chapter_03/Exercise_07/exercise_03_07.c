41a64c8b0645bc5bb98ec6c7fad5f461deebc2fe
itingh <mk.itingh@gmail.com>
2017-01-08 17:26:13 +0800

    Update
    update (7.16e-6 * 2.01e-8) to (7.16e-6 + 2.01e-8), it might be a typo.

--- Exercises/Chapter_03/Exercise_07/exercise_03_07.c
+++ Exercises/Chapter_03/Exercise_07/exercise_03_07.c
@@ -11,7 +11,7 @@ int main (void)
 {
 	float result;
 
-	result = (3.31e-8 * 2.01e-7) / (7.16e-6 * 2.01e-8);
+	result = (3.31e-8 * 2.01e-7) / (7.16e-6 + 2.01e-8);
 	printf("result = %e", result);
 
 	return 0;
